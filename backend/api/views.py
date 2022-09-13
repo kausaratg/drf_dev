cc272612125663495d1fddfbefd6ec8fe85d079e
kassy <kausaratg@gmail.com>
2022-09-13 22:34:15 +0100

    created get api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,31 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 import json
+from django.forms.models import model_to_dict
+from products.models import Product
+from rest_framework.decorators import api_view 
+from rest_framework.response import Response 
+from products.serializers import ProductSerializer
 
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
-    body = request.body
+    # print(request.GET)
+    # body = request.body
+    # data = {}
+    # try:
+    #     data = json.loads(body)
+    # except:
+    #     pass
+    # print(data)
+    # data['params'] = dict(request.GET)
+    # data['headers'] = dict(request.headers)
+    # print(request.headers)
+    # data['content_type'] = request.content_type
+    # # return JsonResponse({"message": "Hi there this is your django api response"})
+    # return JsonResponse(data)
+    # if request.method != "POST":
+    #     return Response({"detail":'Get not allowed'}, status=405)
+    instance = Product.objects.all().order_by("?").last()
     data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    print(data.keys())
-    # data['headers'] = request.headers
-    print(request.headers)
-    data['content_type'] = request.content_type
-    return JsonResponse({"message": "Hi there this is your django api response"})
+    if instance:
+        data = ProductSerializer(instance).data
+    return Response(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -6,6 +6,9 @@ endpoint = 'https://httpbin.org/anything'
 endpoint = "http://127.0.0.1:8000/api/"
 
 get_response = requests.get(endpoint, params={"abc":123}, json={'query':'I am kassy. world'})
-# print(get_response.text)
-print(get_response.json())
+# get_response = requests.get(endpoint)
+print(get_response.text)
+# print(get_response.headers)
+# print(get_response.json())
+
 # print(get_response.status_code)
